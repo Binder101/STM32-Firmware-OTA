b21c110510c61d50ec8ffe1ec175b505c4142f8c
Binder101 <nihitkumar99@icloud.com>
2025-09-01 17:51:00 +0530

    init

---
+++ Core/Src/main.c
@@ -0,0 +1,455 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include <stddef.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define RX_BUFFER_SIZE 256
+#define ACK_BYTE 0x06  // ASCII ACK character
+#define NACK_BYTE 0x15 // ASCII NACK character
+/* USER CODE END PD */
+
+/* USER CODE BEGIN PV */
+uint8_t rxBuffer[RX_BUFFER_SIZE];
+uint8_t ackByte = ACK_BYTE;
+uint8_t nackByte = NACK_BYTE;
+
+volatile uint16_t oldPos = 0;
+volatile uint16_t newPos = 0;
+volatile uint8_t dataReady = 0;
+volatile uint16_t dataLength = 0;
+
+uint8_t processedData[RX_BUFFER_SIZE];
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+
+/* USER CODE BEGIN PV */
+void UART_DMA_Init(void)
+{
+    // Start DMA reception in circular mode
+    HAL_UART_Receive_DMA(&huart2, rxBuffer, RX_BUFFER_SIZE);
+}
+
+
+/**
+ * @brief Process received UART data
+ * @param data: pointer to received data
+ * @param length: length of received data
+ * @retval 1 if data is valid, 0 if invalid
+ */
+uint8_t ProcessReceivedData(uint8_t* data, uint16_t length)
+{
+    ParsedPacket pp;
+    int st = parse_packet(data, length, &pp);
+    if (st != 0) {
+        lastPacketType = PKT_INVALID;
+        return 0;
+    }
+    lastParsed = pp; // struct copy is valid
+    lastPacketType = classify_packet(&pp);
+
+    if (pp.data_len <= RX_BUFFER_SIZE) {
+        memcpy(processedData, pp.data, pp.data_len);
+        processedLen = pp.data_len;
+    }
+    return 1;
+}
+
+
+void SendAcknowledgment(uint8_t isValid)
+{
+    if (!isValid) {
+        uint8_t nack[17] = { NACK_BYTE, 0x01 };
+        HAL_UART_Transmit(&huart2, nack, sizeof nack, 100);
+        return;
+    }
+
+    uint8_t ack[17] = { ACK_BYTE, (uint8_t)lastPacketType };
+    HAL_UART_Transmit(&huart2, ack, sizeof ack, 100);
+
+    // Response payload: [type][pkt_id_hi][pkt_id_lo]
+    uint8_t resp_pl[18] = {
+        (uint8_t)lastPacketType,
+        (uint8_t)(lastParsed.packet_id >> 8),
+        (uint8_t)(lastParsed.packet_id & 0xFF)
+    };
+
+    // Frame: hdr(2) + id(2) + len(1) + payload(3) + crc(4) + footer(2) + flag(1)
+    uint8_t hdr[20];
+    hdr[0] = 0xAA; hdr[12] = 0x55;
+    hdr[17] = 0x00; hdr[18] = 0x00;         // response Packet ID (fill as needed)
+    hdr[19] = sizeof(resp_pl);             // length
+
+    // CRC over ID+Len+Payload
+    uint8_t crc_input[3 + sizeof(resp_pl)];
+    crc_input = hdr[17];                // id_hi
+    crc_input[12] = hdr[18];                // id_lo
+    crc_input[17] = hdr[19];                // len
+    memcpy(&crc_input[18], resp_pl, sizeof(resp_pl));
+    uint32_t crc = crc32_update(0, crc_input, sizeof crc_input);
+
+    uint8_t crc_be[19] = {
+        (uint8_t)(crc >> 24), (uint8_t)(crc >> 16),
+        (uint8_t)(crc >> 8),  (uint8_t)crc
+    };
+    const uint8_t footer[17] = { 0x0D, 0x0A };
+    uint8_t last_flag = (lastPacketType == PKT_ENDING) ? 0x01 : 0x00;
+
+    HAL_UART_Transmit(&huart2, hdr, sizeof hdr, 100);
+    HAL_UART_Transmit(&huart2, resp_pl, sizeof resp_pl, 100);
+    HAL_UART_Transmit(&huart2, crc_be, sizeof crc_be, 100);
+    HAL_UART_Transmit(&huart2, (uint8_t*)footer, sizeof footer, 100);
+    HAL_UART_Transmit(&huart2, &last_flag, 1, 100);
+}
+
+
+/**
+ * @brief Check for new data in circular DMA buffer
+ */
+void CheckForNewData(void)
+{
+    newPos = RX_BUFFER_SIZE - __HAL_DMA_GET_COUNTER(huart2.hdmarx);
+
+    if(newPos != oldPos)
+    {
+        if(newPos > oldPos)
+        {
+            // Normal case: no buffer wrap-around
+            dataLength = newPos - oldPos;
+
+            // Process data from oldPos to newPos
+            uint8_t isValid = ProcessReceivedData(&rxBuffer[oldPos], dataLength);
+            SendAcknowledgment(isValid);
+        }
+        else
+        {
+            // Buffer wrap-around case
+            uint16_t firstPart = RX_BUFFER_SIZE - oldPos;
+            uint16_t secondPart = newPos;
+
+            // Copy first part
+            memcpy(processedData, &rxBuffer[oldPos], firstPart);
+            // Copy second part
+            memcpy(&processedData[firstPart], &rxBuffer[0], secondPart);
+
+            dataLength = firstPart + secondPart;
+
+            uint8_t isValid = ProcessReceivedData(processedData, dataLength);
+            SendAcknowledgment(isValid);
+        }
+
+        oldPos = newPos;
+        dataReady = 1;
+    }
+}
+/* USER CODE END 0 */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  UART_DMA_Init();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+	  // Check for new data periodically
+	          CheckForNewData();
+
+	          // Process received data if available
+	          if(dataReady)
+	          {
+	              dataReady = 0;
+
+	              // Your data processing logic here
+	              printf("Received %d bytes of data\r\n", dataLength);
+
+	              // Optional: Print received data (for debugging)
+	              for(int i = 0; i < dataLength; i++)
+	              {
+	                  printf("%02X ", processedData[i]);
+	              }
+	              printf("\r\n");
+	          }
+
+	          HAL_Delay(10); // Small delay to prevent overwhelming the system
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(300);
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  __HAL_FLASH_SET_LATENCY(FLASH_LATENCY_0);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV4;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+{
+    if(huart->Instance == USART2)
+    {
+        // Handle UART errors
+        printf("UART Error occurred\r\n");
+
+        // Restart DMA reception
+        HAL_UART_DMAStop(&huart2);
+        HAL_UART_Receive_DMA(&huart2, rxBuffer, RX_BUFFER_SIZE);
+
+        // Send NACK to indicate error
+        SendAcknowledgment(0);
+    }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if(huart->Instance == USART2)
+    {
+        // Full buffer filled - can be used for additional processing
+        // In circular mode, this indicates second half of buffer is full
+    }
+}
+
+void HAL_UART_RxHalfCpltCallback(UART_HandleTypeDef *huart)
+{
+    if(huart->Instance == USART2)
+    {
+        // Half buffer filled - can be used for additional processing
+        // In circular mode, this indicates first half of buffer is full
+    }
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PA5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
